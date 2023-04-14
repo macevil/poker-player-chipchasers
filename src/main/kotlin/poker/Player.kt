86e0f72aa4f7e8fa1d880e906f35d7cc21c1ec51
Johannes Jungkunst <kaffeemann@outlook.com>
2023-04-14 12:13:55 +0000

    vielleicht jetzt

--- src/main/kotlin/poker/Player.kt
+++ src/main/kotlin/poker/Player.kt
@@ -6,24 +6,28 @@ import org.json.JSONArray
 class Player {
 
     fun betRequest(game_state: JSONObject): Int {
-        println("chipchasers0")
+        try{
+            println("chipchasers0")
 
-        println(game_state.get("community_cards"))
-        game_state.get("in_action")
-        val players = game_state.getJSONArray("players")
-        for (i in 0 until players.length()) {
-            val player = players.getJSONObject(i)
-            println("${player.get("bet")}, ${player.get("stack")}, ${player.get("name")}")
-            if(player.get("name")=="chipchasers"){
-                val cards = player.getJSONArray("hole_cards")
-                for (i in 0 until cards.length()) {
-                    val card = cards.getJSONObject(i)
-                    println("${card.get("rank")}, ${card.get("suit")}")
-                } 
-                return player.get("stack")
-            } else{
-                return 0
+            println(game_state.get("community_cards"))
+            game_state.get("in_action")
+            val players = game_state.getJSONArray("players")
+            for (i in 0 until players.length()) {
+                val player = players.getJSONObject(i)
+                println("${player.get("bet")}, ${player.get("stack")}, ${player.get("name")}")
+                if(player.get("name")=="chipchasers"){
+                    val cards = player.getJSONArray("hole_cards")
+                    for (i in 0 until cards.length()) {
+                        val card = cards.getJSONObject(i)
+                        println("${card.get("rank")}, ${card.get("suit")}")
+                    } 
+                    return player.get("stack").toInt()
+                } else{
+                    return 0
+                }
             }
+        } catch (e: Excption){
+return 0
         }
     }
 
