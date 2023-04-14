177bc6aa7e130e0f24292c2b8062b745297b128f
Johannes Jungkunst <kaffeemann@outlook.com>
2023-04-14 12:09:00 +0000

    vielleicht jetzt

--- src/main/kotlin/poker/Player.kt
+++ src/main/kotlin/poker/Player.kt
@@ -14,7 +14,7 @@ class Player {
         for (i in 0 until players.length()) {
             val player = players.getJSONObject(i)
             println("${player.get("bet")}, ${player.get("stack")}, ${player.get("name")}")
-            if(player.get("name")=="chipschasers"){
+            if(player.get("name")=="chipchasers"){
                 val cards = player.getJSONArray("hole_cards")
                 for (i in 0 until cards.length()) {
                     val card = cards.getJSONObject(i)
