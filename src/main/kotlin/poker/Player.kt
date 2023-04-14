fc676e56fd0761d86f55dfae90bd8837b4660b8e
Johannes Jungkunst <kaffeemann@outlook.com>
2023-04-14 09:21:07 +0000

    kappes

--- src/main/kotlin/poker/Player.kt
+++ src/main/kotlin/poker/Player.kt
@@ -7,13 +7,7 @@ class Player {
     fun betRequest(game_state: JSONObject): Int {
         println("chipchasers0")
         JSONArray pets = game_state.getJSONArray("players")
-        // JSONArray cards = pets[1].getJSONArray("hole_cards")
-        println(game_state.getString("round"))
-        println(game_state.getString("bet_index"))
-        println(game_state.getString("small_blind"))
-        println(game_state.getString("orbits"))
-        println(game_state.getString("dealer"))
-        println(game_state.getString("pot"))
+        JSONArray cards = pets[1].getJSONArray("hole_cards")
         return 5
     }
 
