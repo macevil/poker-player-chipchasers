91f13598c91ec5f18788159f3ec6d876e41f9525
Johannes Jungkunst <kaffeemann@outlook.com>
2023-04-14 08:19:37 +0000

    1

--- src/main/kotlin/poker/Player.kt
+++ src/main/kotlin/poker/Player.kt
@@ -3,7 +3,9 @@ package poker
 import org.json.JSONObject
 
 class Player {
+
     fun betRequest(game_state: JSONObject): Int {
+        println(game_state.players[1].hole_cards)
         return 0
     }
 
