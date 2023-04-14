bdb4a634abfa09a3e38d447699c4861cdd5e00e4
Johannes Jungkunst <kaffeemann@outlook.com>
2023-04-14 09:55:42 +0000

    ah

--- src/main/kotlin/poker/Player.kt
+++ src/main/kotlin/poker/Player.kt
@@ -7,7 +7,7 @@ class Player {
 
     fun betRequest(game_state: JSONObject): Int {
         println("chipchasers0")
-        val str = String(game_state.getBytes())
+        val str = String(game_state)
         return 100
     }
 
