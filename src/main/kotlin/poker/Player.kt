223a08a2037896b18075a129004633bd708cdc2d
Johannes Jungkunst <kaffeemann@outlook.com>
2023-04-14 10:36:46 +0000

    ah

--- src/main/kotlin/poker/Player.kt
+++ src/main/kotlin/poker/Player.kt
@@ -5,7 +5,6 @@ import org.json.JSONArray
 
 class Player {
 
-    @RequestMapping(value = "/betRequest", method = RequestMethod.POST, consumes="application/json")
     fun betRequest(game_state: JSONObject): Int {
         println("chipchasers0")
         return 100
--- src/main/kotlin/poker/PlayerService.kt
+++ src/main/kotlin/poker/PlayerService.kt
@@ -13,6 +13,7 @@ fun main(args: Array<String>) {
         val result = when (action) {
             "bet_request" -> {
                 val gameState = request.bodyParams["game_state"]
+                println(request.bodyParams)
 
                 if (gameState == null) {
                     "Missing game_state!"
